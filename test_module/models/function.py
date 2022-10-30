4e73325105e17325dac689394924676f78c38cdc
Tenager-Pro <65409198+Tenager-Pro@users.noreply.github.com>
2022-10-30 13:58:45 +0300

    Delete function.py

--- test_module/models/function.py
+++
@@ -1,5 +0,0 @@
-def checkFunc(self, check_id,value):
-        if self.env['test.model.lines'].search([('client_name','=',check_id)]):
-            raise ValidationError(_("Этот пользователь уже существует"))
-        else:
-            self.env['test.model.lines'].create(value)   
